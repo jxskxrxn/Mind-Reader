31adfce850db732349cde687d225ce81a227b05c
jxskxrxn <79559600+jxskxrxn@users.noreply.github.com>
2021-05-12 14:23:14 +0800

    Update mind_reader_using_pydealer.py

--- mind_reader_using_pydealer.py
+++ mind_reader_using_pydealer.py
@@ -11,6 +11,7 @@ deck.shuffle()
 
 '''initial 4 cards'''
 dealt_cards = deck.deal(4)
+memory = input("Memorise a Card from the given options and i will make it disappear!\nPress the enter key when you are ready! ")
 
 '''new hand with extra random card added'''
 hand.add(dealt_cards[0:3])
@@ -18,8 +19,13 @@ hand.add(deck.deal(1))
 hand.shuffle()
 
 
-
-print("old hand")
 print(dealt_cards)
-print("new hand")
+
+mindFreak = input("Did I manage to remove the card you were thinking of?(y or n): ")
+
 print(hand)
+
+if mindFreak == "y":
+  print("I am the best Mind-Reader!!!")
+else:
+  print("I will try again :(")
