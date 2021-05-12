f218968909b1511c82ca96235f22b47fe5097ebd
jxskxrxn <79559600+jxskxrxn@users.noreply.github.com>
2021-05-12 15:05:16 +0800

    Update mind_reader_using_pydealer.py
    Added comments and made small changes

--- mind_reader_using_pydealer.py
+++ mind_reader_using_pydealer.py
@@ -1,29 +1,38 @@
+# External module import
 import pydealer
 
-#mind reader using pydealer module
-
+# initialise a deck of cards 
 deck = pydealer.Deck()
+
+# initialise a second deck of cards called a "hand"
 hand = pydealer.Stack()
 
-'''shuffle deck'''
+# shuffle the deck
 deck.shuffle()
 
-
-'''initial 4 cards'''
+# randomly select 4 cards from the intial deck 
 dealt_cards = deck.deal(4)
+
+# print the initial deck
+print(dealt_cards)
+
 memory = input("Memorise a Card from the given options and i will make it disappear!\nPress the enter key when you are ready! ")
 
-'''new hand with extra random card added'''
+# second deck of card uses 3 cards from the initial deck 
 hand.add(dealt_cards[0:3])
+ 
+# adds one random card to the new deck
 hand.add(deck.deal(1))
+
+# shuffle the second deck 
 hand.shuffle()
 
+# print the second deck 
+print(hand)
 
-print(dealt_cards)
 
 mindFreak = input("Did I manage to remove the card you were thinking of?(y or n): ")
 
-print(hand)
 
 if mindFreak == "y":
   print("I am the best Mind-Reader!!!")
